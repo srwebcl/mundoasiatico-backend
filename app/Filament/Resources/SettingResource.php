c9074fab9b235c1aecfac784ad90c3e62e62e5f9
srwebcl <contacto@srweb.cl>
2026-05-22 18:03:39 -0400

    fix: renombrar menu whatsapp y limpiar cache filament

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -18,9 +18,9 @@ class SettingResource extends Resource
     protected static ?string $model = Setting::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-cog-8-tooth';
-    protected static ?string $navigationLabel = 'Ajustes del Sitio';
+    protected static ?string $navigationLabel = 'WhatsApp y Ajustes';
     protected static ?string $modelLabel = 'Ajuste';
-    protected static ?string $pluralModelLabel = 'Ajustes';
+    protected static ?string $pluralModelLabel = 'Ajustes Generales';
     protected static ?string $navigationGroup = 'Configuración';
 
     public static function form(Form $form): Form
